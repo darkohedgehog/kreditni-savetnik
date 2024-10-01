99c4467dc3cc6567229cdd9278db7f873f58da3a
Darko Živić <zivic.darko79@gmail.com>
2024-10-01 22:28:59 +0200

    "Added WobbleCardSection component to Home page"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { HeroSectionWithBeamsAndGrid } from "@/components/home/HeroSectionWithBeamsAndGrid";
+import { WobbleCardSection } from "@/components/home/WobbleCardSection";
 
 
 export default function Home() {
   return (
    <>
    <HeroSectionWithBeamsAndGrid />
+   <WobbleCardSection  />
    </>
   );
 }
