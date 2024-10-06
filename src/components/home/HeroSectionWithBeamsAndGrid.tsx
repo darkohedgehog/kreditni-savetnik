d8ef9a05a9860b89ad873a6bfd0d3bbbafd1f29c
Darko Živić <zivic.darko79@gmail.com>
2024-10-06 15:28:04 +0200

    "Update image source in HeroSectionWithBeamsAndGrid component"

--- src/components/home/HeroSectionWithBeamsAndGrid.tsx
+++ src/components/home/HeroSectionWithBeamsAndGrid.tsx
@@ -42,7 +42,7 @@ export function HeroSectionWithBeamsAndGrid() {
       >
         <div className="rounded-[24px] border border-neutral-200 bg-white p-2 dark:border-neutral-700 dark:bg-black">
           <Image
-            src="https://assets.aceternity.com/pro/aceternity-landing.webp"
+            src="/savetnica.webp"
             alt="header"
             width={1920}
             height={1080}
