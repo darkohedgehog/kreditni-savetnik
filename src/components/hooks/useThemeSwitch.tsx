c3f7314bc335e2f5bfae54399be401bf20e03347
Darko Živić <zivic.darko79@gmail.com>
2024-10-08 19:07:52 +0200

    "Updated theme switcher icons and menu item styles with additional classes for color and text styling."

--- src/components/hooks/useThemeSwitch.tsx
+++ src/components/hooks/useThemeSwitch.tsx
@@ -19,26 +19,26 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <MdOutlineLightMode className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MdOutlineDarkMode className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <MdOutlineLightMode className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-yellow-400" />
+          <MdOutlineDarkMode className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-cyan-100" />
           <span className="sr-only"></span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem 
-        className='gap-1 flex' 
+        className='gap-1 flex text-darkblue dark:text-accentblue' 
         onClick={() => setTheme("light")}>
           <MdOutlineLightMode />
           Svetla
         </DropdownMenuItem>
         <DropdownMenuItem 
-        className='gap-1 flex' 
+        className='gap-1 flex text-darkblue dark:text-accentblue' 
         onClick={() => setTheme("dark")}>
           <MdOutlineDarkMode />
           Tamna
         </DropdownMenuItem>
         <DropdownMenuItem 
-        className='gap-1 flex' 
+        className='gap-1 flex text-darkblue dark:text-accentblue' 
         onClick={() => setTheme("system")}>
           <MdOutlineSystemSecurityUpdateGood />
           Sistem
