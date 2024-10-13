77e680629079d550f71eee6766083d3c648c1f1e
Darko Živić <zivic.darko79@gmail.com>
2024-10-13 18:44:15 +0200

    "Added Link component from 'next/link' to wrap Logo component in NavTop"

--- src/components/navigation/NavTop.tsx
+++ src/components/navigation/NavTop.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Logo from '../logo/Logo'
 import { ModeToggle } from '../hooks/useThemeSwitch'
+import Link from 'next/link'
 
 const NavTop = () => {
   return (
     <div className='flex items-center justify-between gap-10 w-full z-20 mt-4 px-4'>
+      <Link  href='/'>
         <Logo />
+      </Link>
         <ModeToggle />
     </div>
   )
